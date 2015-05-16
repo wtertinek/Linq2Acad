8f8a7acb3ca5b4efb940c8d071f6f74f880b3d7c
Wolfgang Tertinek <wolfgang.tertinek@gmx.at>
2015-05-16 10:15:35 +0200

    NoDatabaseDefaults as parameter

---
+++ Linq2AcDb/Extensions/TableRecord/BlockTableRecordExtensions.cs
@@ -0,0 +1,67 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq2AcDb
+{
+  public static class BlockTableRecordExntensions
+  {
+    public static IEnumerable<Entity> Items(this BlockTableRecord source)
+    {
+      Helpers.CheckTransaction();
+      return AcdbEnumerable<Entity>.Create(ActiveDatabase.Transaction, source.Id);
+    }
+
+    public static IEnumerable<ObjectId> Cast(this BlockTableRecord source)
+    {
+      foreach (var id in source)
+      {
+        yield return id;
+      }
+    }
+
+    public static IEnumerable<T> OfType<T>(this BlockTableRecord source) where T : Entity
+    {
+      Helpers.CheckTransaction();
+      return AcdbEnumerable<T>.Create(ActiveDatabase.Transaction, source.Id, true);
+    }
+
+    public static ObjectId Add<T>(this BlockTableRecord source, T item) where T : Entity
+    {
+      return Add<T>(source, item, false);
+    }
+
+    public static ObjectId Add<T>(this BlockTableRecord source, T item, bool noDatabaseDefaults) where T : Entity
+    {
+      Helpers.CheckTransaction();
+      return Helpers.WriteCheck(source, () => AddItem(source, item, noDatabaseDefaults));
+    }
+
+    public static IEnumerable<ObjectId> AddRange<T>(this BlockTableRecord source, IEnumerable<T> items) where T : Entity
+    {
+      return AddRange<T>(source, items, false);
+    }
+
+    public static IEnumerable<ObjectId> AddRange<T>(this BlockTableRecord source, IEnumerable<T> items, bool noDatabaseDefaults) where T : Entity
+    {
+      Helpers.CheckTransaction();
+      return Helpers.WriteCheck(source, () => items.Select(i => AddItem(source, i, noDatabaseDefaults))
+                                                   .ToArray());
+    }
+
+    private static ObjectId AddItem(BlockTableRecord btr, Entity item, bool noDatabaseDefaults)
+    {
+      if (!noDatabaseDefaults)
+      {
+        item.SetDatabaseDefaults();
+      }
+
+      var id = btr.AppendEntity(item);
+      ActiveDatabase.Transaction.Value.AddNewlyCreatedDBObject(item, true);
+      return id;
+    }
+  }
+}
