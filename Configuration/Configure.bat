ef4d3375fc9f0dae69af189b085d57536515ae9f
Wolfgang Tertinek <wolfgang.tertinek@gmail.com>
2016-07-31 15:08:54 +0200

    Configuration script added

---
+++ Configuration/Configure.bat
@@ -0,0 +1,1 @@
+Powershell.exe -executionpolicy remotesigned -File Configure.ps1
---
+++ Configuration/Configure.ps1
@@ -0,0 +1,16 @@
+$folder = Read-Host 'Please enter your AutoCAD installation folder'
+
+if (Test-Path $folder)
+{
+  (Get-Content -Encoding UTF8 Template.csproj.user) | ForEach-Object { $_ -replace "{AcadRootDir}", "$folder" } | Set-Content -Encoding UTF8 ..\Linq2Acad\Linq2Acad.csproj.user
+  (Get-Content -Encoding UTF8 Template.csproj.user) | ForEach-Object { $_ -replace "{AcadRootDir}", "$folder" } | Set-Content -Encoding UTF8 ..\Linq2Acad.Examples\Linq2Acad.Examples.csproj.user
+  (Get-Content -Encoding UTF8 Template.csproj.user) | ForEach-Object { $_ -replace "{AcadRootDir}", "$folder" } | Set-Content -Encoding UTF8 ..\Linq2Acad.Tests\Linq2Acad.Tests.csproj.user
+  (Get-Content -Encoding UTF8 Template.csproj.user) | ForEach-Object { $_ -replace "{AcadRootDir}", "$folder" } | Set-Content -Encoding UTF8 ..\Linq2Acad.Tests.Acad\Linq2Acad.Tests.Acad.csproj.user
+  (Get-Content -Encoding UTF8 ..\AcadTestRunner\AcadTestRunner.dll.config) | ForEach-Object { $_ -replace "{AcadRootDir}", "$folder" } | Set-Content -Encoding UTF8 ..\AcadTestRunner\AcadTestRunner.dll.config
+  
+  Write-Host 'AutoCAD reference path set to' $folder
+}
+else
+{
+  Write-Host $folder 'does not exist'
+}
