9cea7bdaa6dd529a5f7742b37221af4596d9bb6b
Wolfgang Tertinek <wolfgang.tertinek@gmail.com>
2016-10-17 23:39:14 +0200

    Test configuration fixed

--- Configuration/Configure.ps1
+++ Configuration/Configure.ps1
@@ -37,6 +37,13 @@ if (Test-Path $acadRootDir)
   Update-File ..\Linq2Acad.SampleCode.CS\Linq2Acad.SampleCode.CS.csproj.user "DefaultTemplate.csproj.user" "{AcadRootDir}" "$acadRootDir"
   Update-File ..\Linq2Acad.SampleCode.VB\Linq2Acad.SampleCode.VB.vbproj.user "DefaultTemplate.vbproj.user" "{AcadRootDir}" "$acadRootDir"
   Update-File ..\Linq2Acad.Tests\Linq2Acad.Tests.csproj.user "TestsTemplate.csproj.user" "{AcadRootDir}" "$acadRootDir"
+
+  if (Test-Path ..\Linq2Acad.Tests\Linq2Acad.Tests/Libraries/AcadTestRunner.dll.config)
+  {
+    Remove-Item ..\Linq2Acad.Tests\Linq2Acad.Tests/Libraries/AcadTestRunner.dll.config
+  }
+  
+  Update-File ..\Linq2Acad.Tests\Linq2Acad.Tests/Libraries/AcadTestRunner.dll.config "AcadTestRunner.dll.config" "{AcadRootDir}" "$acadRootDir"
   
   Write-Host 'AutoCAD reference path set to' $acadRootDir
 }
