bf147b4735fc5c31ac72f51e3fb94885df76d96e
Wolfgang Tertinek <wolfgang.tertinek@gmail.com>
2017-05-26 10:20:16 +0200

    Extension methods for AttributeCollection and DynamicBlockReferencePropertyCollection added

---
+++ Linq2Acad/Extensions/AttributeCollectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace Linq2Acad
+{
+  public static class AttributeCollectionExtensions
+  {
+    public static bool Contains(this AttributeCollection attributes, string tag)
+    {
+      return attributes.Cast<AttributeReference>()
+                       .Any(a => a.Tag == tag);
+    }
+
+    public static string GetValue(this AttributeCollection attributes, string tag)
+    {
+      var attribute = attributes.Cast<AttributeReference>()
+                                .FirstOrDefault(a => a.Tag == tag);
+
+      if (attribute == null)
+      {
+        throw Error.KeyNotFound(tag);
+      }
+      else
+      {
+        return attribute.TextString;
+      }
+    }
+
+    public static void SetValue(this AttributeCollection attributes, string tag, string value)
+    {
+      var attribute = attributes.Cast<AttributeReference>()
+                                .FirstOrDefault(a => a.Tag == tag);
+
+      if (attribute == null)
+      {
+        throw Error.KeyNotFound(tag);
+      }
+      else
+      {
+        attribute.TextString = value;
+      }
+    }
+  }
+}
---
+++ Linq2Acad/Extensions/DynamicBlockReferencePropertyCollectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace Linq2Acad
+{
+  public static class DynamicBlockReferencePropertyCollectionExtensions
+  {
+    public static bool Contains(this DynamicBlockReferencePropertyCollection properties, string name)
+    {
+      return properties.Cast<DynamicBlockReferenceProperty>()
+                       .Any(p => p.PropertyName == name);
+    }
+
+    public static object GetValue(this DynamicBlockReferencePropertyCollection properties, string name)
+    {
+      var property = properties.Cast<DynamicBlockReferenceProperty>()
+                               .FirstOrDefault(p => p.PropertyName == name);
+
+      if (property == null)
+      {
+        throw Error.KeyNotFound(name);
+      }
+      else
+      {
+        return property.Value;
+      }
+    }
+
+    public static void SetValue(this DynamicBlockReferencePropertyCollection properties, string name, object value)
+    {
+      var property = properties.Cast<DynamicBlockReferenceProperty>()
+                               .FirstOrDefault(a => a.PropertyName == name);
+
+      if (property == null)
+      {
+        throw Error.KeyNotFound(name);
+      }
+      else
+      {
+        property.Value = value;
+      }
+    }
+  }
+}
