b95c0ac451ee84b8c7971d7891ad70446e3b5d01
Wolfgang Tertinek <wolfgang.tertinek@gmail.com>
2018-05-21 22:58:08 +0200

    Folders renamed from 201x to AutoCAD_201x to avoid misinterpretation as Visual  Studio version numbers

--- 2016/packages/AutoCAD.NET.Core.20.1.0/tools/install.ps1
+++
@@ -1,8 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-foreach ($reference in $project.Object.References)
-{
-    if($reference.Name -eq "accoremgd")
-    {
-        $reference.CopyLocal = $false;
-    }
-}
--- 2016/packages/AutoCAD.NET.Model.20.1.0/tools/install.ps1
+++
@@ -1,9 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-foreach ($reference in $project.Object.References)
-{
-    if($reference.Name -eq "acdbmgd" -Or
-       $reference.Name -eq "acdbmgdbrep" )
-    {
-        $reference.CopyLocal = $false;
-    }
-}
