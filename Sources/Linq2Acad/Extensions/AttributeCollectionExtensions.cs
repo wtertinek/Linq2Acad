a54dcf4cfbe8cfc84b19b3c253dfa3c9ab4e38a2
Wolfgang Tertinek <wolfgang.tertinek@gmail.com>
2021-01-27 23:29:07 +0100

    Attribute extensions fixed

--- Sources/Linq2Acad/Extensions/AttributeCollectionExtensions.cs
+++ Sources/Linq2Acad/Extensions/AttributeCollectionExtensions.cs
@@ -11,34 +11,51 @@ namespace Linq2Acad
   {
     public static bool Contains(this AttributeCollection attributes, string tag)
     {
-      Require.ParameterNotNull(attributes, nameof(attributes));
-
-      return attributes.Cast<AttributeReference>()
-                       .Any(a => a.Tag == tag);
+      return GetAttributeReferences(attributes, OpenMode.ForRead)
+             .Any(a => a.Tag == tag);
     }
 
     public static string GetValue(this AttributeCollection attributes, string tag)
     {
-      Require.ParameterNotNull(attributes, nameof(attributes));
-
-      var attribute = attributes.Cast<AttributeReference>()
-                                .FirstOrDefault(a => a.Tag == tag);
-
-      Require.ObjectNotNull(attribute, $"No {nameof(AttributeReference)} with Tag '{tag}' found");
-
+      var attribute = GetAttributeReference(attributes, tag, OpenMode.ForRead);
       return attribute.TextString;
     }
 
     public static void SetValue(this AttributeCollection attributes, string tag, string value)
     {
-      Require.ParameterNotNull(attributes, nameof(attributes));
+      var attribute = GetAttributeReference(attributes, tag, OpenMode.ForWrite);
+      attribute.TextString = value;
+    }
 
-      var attribute = attributes.Cast<AttributeReference>()
-                                .FirstOrDefault(a => a.Tag == tag);
+    private static AttributeReference GetAttributeReference(AttributeCollection attributes, string tag, OpenMode openMode)
+    {
+      var attribute = GetAttributeReferences(attributes, openMode)
+                      .FirstOrDefault(a => a.Tag == tag);
 
       Require.ObjectNotNull(attribute, $"No {nameof(AttributeReference)} with Tag '{tag}' found");
 
-      attribute.TextString = value;
+      return attribute;
+    }
+
+    private static IEnumerable<AttributeReference> GetAttributeReferences(AttributeCollection attributes, OpenMode openMode)
+    {
+      Require.ParameterNotNull(attributes, nameof(attributes));
+
+      if (attributes.Count > 0)
+      {
+        if (attributes[0].Database.TransactionManager.TopTransaction == null)
+        {
+          throw new InvalidOperationException("No transaction available");
+        }
+
+        var transaction = attributes[0].Database.TransactionManager.TopTransaction;
+        return attributes.Cast<ObjectId>()
+                         .Select(id => (AttributeReference)transaction.GetObject(id, openMode));
+      }
+      else
+      {
+        return Array.Empty<AttributeReference>();
+      }
     }
   }
 }
