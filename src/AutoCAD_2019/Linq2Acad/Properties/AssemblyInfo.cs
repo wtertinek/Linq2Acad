1c62a8b693d0b8e4a8da3d56855d6595ea6c510c
Wolfgang Tertinek <wolfgang.tertinek@gmail.com>
2022-05-04 23:53:28 +0200

    AssemblyInfos now per project

--- src/AutoCAD_2019/Linq2Acad/Properties/AssemblyInfo.cs
+++ src/AutoCAD_2019/Linq2Acad/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Linq2Acad.Tests")]
+[assembly: AssemblyTitle("Linq2Acad 2019")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Linq2Acad")]
-[assembly: AssemblyCopyright("Copyright © 2022")]
+[assembly: AssemblyProduct("Linq2Acad 2019")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3bb52cdf-3774-4569-8445-90ecbf398ef2")]
+[assembly: Guid("1a282c1c-7b1e-480f-8a4d-6bab4a21da04")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.11.0.0")]
-[assembly: AssemblyFileVersion("0.11.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/AutoCAD_2021/Linq2Acad/Properties/AssemblyInfo.cs
+++ src/AutoCAD_2021/Linq2Acad/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Linq2Acad")]
+[assembly: AssemblyTitle("Linq2Acad 2021")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Linq2Acad")]
+[assembly: AssemblyProduct("Linq2Acad 2021")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
