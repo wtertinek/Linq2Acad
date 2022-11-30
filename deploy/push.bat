00b9475eb6bf1aec0de18adb3234e88b3c409742
Wolfgang Tertinek <wolfgang.tertinek@gmail.com>
2022-11-30 12:01:18 +0100

    ci: Move nuspec files to new folder /deploy
    Add batch files for packing and pushing

---
+++ deploy/push.bat
@@ -0,0 +1,12 @@
+set VERSION=%1
+set API_KEY=%2
+
+nuget push Linq2Acad-2015.%VERSION%.nupkg %API_KEY% -Source https://api.nuget.org/v3/index.json
+nuget push Linq2Acad-2016.%VERSION%.nupkg %API_KEY% -Source https://api.nuget.org/v3/index.json
+nuget push Linq2Acad-2017.%VERSION%.nupkg %API_KEY% -Source https://api.nuget.org/v3/index.json
+nuget push Linq2Acad-2018.%VERSION%.nupkg %API_KEY% -Source https://api.nuget.org/v3/index.json
+nuget push Linq2Acad-2019.%VERSION%.nupkg %API_KEY% -Source https://api.nuget.org/v3/index.json
+nuget push Linq2Acad-2020.%VERSION%.nupkg %API_KEY% -Source https://api.nuget.org/v3/index.json
+nuget push Linq2Acad-2021.%VERSION%.nupkg %API_KEY% -Source https://api.nuget.org/v3/index.json
+nuget push Linq2Acad-2022.%VERSION%.nupkg %API_KEY% -Source https://api.nuget.org/v3/index.json
+nuget push Linq2Acad-2023.%VERSION%.nupkg %API_KEY% -Source https://api.nuget.org/v3/index.json
