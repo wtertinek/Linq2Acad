8adbdf7c818637c02a1ae4bff50fa85a62457656
Wolfgang Tertinek <wolfgang.tertinek@gmail.com>
2022-11-30 11:53:48 +0100

    refactor: Replace version-specific C# projects by single project
    Replace version-specific projects by a single project that contains all versions. New project contains Debug_20xx and Release_20xx constants per AutoCAD version. Framwork version and AutoCAD reference are controlled via conditions in the project file.

--- src/Linq2Acad/Properties/AssemblyInfo.cs
+++ src/Linq2Acad/Properties/AssemblyInfo.cs
@@ -5,11 +5,47 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+#if AutoCAD_2015
+[assembly: AssemblyTitle("Linq2Acad 2015")]
+#elif AutoCAD_2016
+[assembly: AssemblyTitle("Linq2Acad 2016")]
+#elif AutoCAD_2017
+[assembly: AssemblyTitle("Linq2Acad 2017")]
+#elif AutoCAD_2018
+[assembly: AssemblyTitle("Linq2Acad 2018")]
+#elif AutoCAD_2019
+[assembly: AssemblyTitle("Linq2Acad 2019")]
+#elif AutoCAD_2020
+[assembly: AssemblyTitle("Linq2Acad 2020")]
+#elif AutoCAD_2021
 [assembly: AssemblyTitle("Linq2Acad 2021")]
+#elif AutoCAD_2022
+[assembly: AssemblyTitle("Linq2Acad 2022")]
+#elif AutoCAD_2023
+[assembly: AssemblyTitle("Linq2Acad 2023")]
+#endif
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
+#if AutoCAD_2015
+[assembly: AssemblyProduct("Linq2Acad 2015")]
+#elif AutoCAD_2016
+[assembly: AssemblyProduct("Linq2Acad 2016")]
+#elif AutoCAD_2017
+[assembly: AssemblyProduct("Linq2Acad 2017")]
+#elif AutoCAD_2018
+[assembly: AssemblyProduct("Linq2Acad 2018")]
+#elif AutoCAD_2019
+[assembly: AssemblyProduct("Linq2Acad 2019")]
+#elif AutoCAD_2020
+[assembly: AssemblyProduct("Linq2Acad 2020")]
+#elif AutoCAD_2021
 [assembly: AssemblyProduct("Linq2Acad 2021")]
+#elif AutoCAD_2022
+[assembly: AssemblyProduct("Linq2Acad 2022")]
+#elif AutoCAD_2023
+[assembly: AssemblyProduct("Linq2Acad 2023")]
+#endif
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
