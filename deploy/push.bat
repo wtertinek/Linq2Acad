9d0eb3a42e7b11612752b2ecfd7dae439b0ce571
Wolfgang Tertinek <wolfgang.tertinek@gmail.com>
2023-04-27 22:28:06 +0200

    chore: change nuget scripts to be executable per version

--- deploy/push.bat
+++ deploy/push.bat
@@ -1,12 +1,5 @@
-set VERSION=%1
-set API_KEY=%2
+set AUTOCAD_VERSION=%1
+set VERSION=%2
+set API_KEY=%3
 
-nuget push Linq2Acad-2015.%VERSION%.nupkg %API_KEY% -Source https://api.nuget.org/v3/index.json
-nuget push Linq2Acad-2016.%VERSION%.nupkg %API_KEY% -Source https://api.nuget.org/v3/index.json
-nuget push Linq2Acad-2017.%VERSION%.nupkg %API_KEY% -Source https://api.nuget.org/v3/index.json
-nuget push Linq2Acad-2018.%VERSION%.nupkg %API_KEY% -Source https://api.nuget.org/v3/index.json
-nuget push Linq2Acad-2019.%VERSION%.nupkg %API_KEY% -Source https://api.nuget.org/v3/index.json
-nuget push Linq2Acad-2020.%VERSION%.nupkg %API_KEY% -Source https://api.nuget.org/v3/index.json
-nuget push Linq2Acad-2021.%VERSION%.nupkg %API_KEY% -Source https://api.nuget.org/v3/index.json
-nuget push Linq2Acad-2022.%VERSION%.nupkg %API_KEY% -Source https://api.nuget.org/v3/index.json
-nuget push Linq2Acad-2023.%VERSION%.nupkg %API_KEY% -Source https://api.nuget.org/v3/index.json
+nuget push %~dp0.\..\packages\Linq2Acad-%AUTOCAD_VERSION%.%VERSION%.nupkg %API_KEY% -Source https://api.nuget.org/v3/index.json
